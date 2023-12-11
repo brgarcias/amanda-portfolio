edce1dd3b41fec8e644fa4d1bd51b930e3879bec
Bruno Garcia <bruno-151299@hotmail.com>
2023-12-11 10:56:10 -0300

    Develop -> Main (#6)
    * Feature/new template (#1)
    
    * refactor: code structure
    
    * improving code structure
    
    * fix: contact page lint
    
    * fix: removing mailchimp variable (#3)
    
    * fix: add node types (#4)
    
    * feat: add feedback contact (#5)

---
+++ src/components/feedback/CircularProgress.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const CircularProgress: FC = () => {
+  return (
+    <div className="circular-progress-container">
+      <div className="circular-progress"></div>
+    </div>
+  );
+};
+
+export default CircularProgress;
